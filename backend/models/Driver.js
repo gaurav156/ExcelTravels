a0623b8843e6f318e6609fda3927123559234594
Gaurav Shukla <gauravshukla156@gmail.com>
2025-05-23 00:29:32 +0530

    Fixes bcrypt not found

--- backend/models/Driver.js
+++ backend/models/Driver.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const DriverSchema = new mongoose.Schema({
   driverId: { type: String, required: true, unique: true },
