f76cf7d8fa0dac6f086f8c3f70694d41af3620ff
Gaurav Shukla <gauravshukla156@gmail.com>
2025-06-15 18:24:53 +0530

    Remove deprecated options

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
